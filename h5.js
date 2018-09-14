f3bd340877af4b3f51bf7dd60d2d8a6d79d3e1ec
Yun <m06m0694ji3@gmail.com>
2018-09-14 18:57:47 +0800

    first

---
+++ h5.js
@@ -0,0 +1,10 @@
+function join(str, concatStr) {
+
+    return str.join("!");
+}
+console.log(join(["a", "b", "c"], "!"));
+
+function repeat(str, times) {
+    return str.repeat(5);
+}
+console.log(repeat('a', 5));
